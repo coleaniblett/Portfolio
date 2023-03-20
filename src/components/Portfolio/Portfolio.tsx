212c5a3d9ad4c3cf3ef203f55cdfaa593d7df80a
ColeANiblett <coleaniblett@gmail.com>
2023-03-20 13:15:30 -0700

    set up placeholder data for PortfolioItem props

--- src/components/Portfolio/Portfolio.tsx
+++ src/components/Portfolio/Portfolio.tsx
@@ -1,10 +1,22 @@
 import React from 'react';
 import { PortfolioItem } from '../PortfolioItem/PortfolioItem';
+import Mockups from '../../assets/DeviceMockups.png';
 
 export const Portfolio: React.FC = () => {
+  const placeHolderItem = {
+    title: "Placeholder Title",
+    technologies: ["Tech1", "Tech2", "Tech3"],
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+  }
+
   return (
     <div>
-      
+      <PortfolioItem 
+        mockups={Mockups} 
+        title={placeHolderItem.title} 
+        tech={placeHolderItem.technologies} 
+        description={placeHolderItem.description}
+      />
     </div>
   );
 }
--- src/components/PortfolioItem/PortfolioItem.tsx
+++ src/components/PortfolioItem/PortfolioItem.tsx
@@ -1,9 +1,22 @@
 import React from 'react';
+import './PortfolioItem.css';
 
-export const PortfolioItem: React.FC = () => {
+interface Props {
+  title: string;
+  mockups: any;
+  tech: string[];
+  description: string;
+}
+
+export const PortfolioItem: React.FC<Props> = ({mockups, title, tech, description}: Props) => {
   return (
     <div>
-
+      <h5>{title}</h5>
+      <ul>
+        {tech.map(technology => (<li>{technology}</li>))}
+      </ul>
+      <p>{description}</p>
+      <img src={mockups}></img>
     </div>
   );
 }
