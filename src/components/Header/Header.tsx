a70928709b1aeab0de5eec6618aafd40480fe839
ColeANiblett <coleaniblett@gmail.com>
2023-04-11 19:29:03 -0700

    add responsive behavior to navbar

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -8,7 +8,13 @@ export const Header: React.FC = () => {
         <p className="title">
           Cole Niblett
         </p>
-        <div>
+        <div className="nav">
+          <ul className="full-menu">
+            <li><a href="#intro-title">About</a></li>
+            <li><a href="#projects">Projects</a></li>
+            <li><a href="#skills">Skills</a></li>
+            <li><a href="#contact">Contact</a></li>
+          </ul>
           <DropdownMenu />
         </div>
     </header>
