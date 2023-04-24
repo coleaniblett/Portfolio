4a16897078e50433060bf855d2564f77440a7d36
ColeANiblett <coleaniblett@gmail.com>
2023-04-24 12:31:50 -0700

    add Education section

---
+++ src/components/Education/Education.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Education.css';
+
+export const Education: React.FC = () => {
+  return (
+    <div className="education">
+      <h6>Education</h6>
+      <p><em>De Anza College | Cupertino, CA</em> (January 2021 – March 2022)<br/>
+      A.A. Systems Programming, GPA 3.87</p>
+      <p><em>San Jose State University | San Jose, CA</em> (August 2012 – June 2016)<br/>
+      B.A. Political SCience, GPA 3.63</p>
+    </div>
+  );
+}
