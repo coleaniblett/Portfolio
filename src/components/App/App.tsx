1b8d4afcabfc40fc440004cec9f030e028bb361c
ColeANiblett <coleaniblett@gmail.com>
2023-04-24 12:39:04 -0700

    add Education section

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,31 +1,37 @@
 import React from 'react';
 import './App.css';
+import { Header } from '../Header/Header';
 import { Hero } from '../Hero/Hero';
 import { Intro } from '../Intro/Intro';
-import { Links } from '../Links/Links';
 import { Portfolio } from '../Portfolio/Portfolio';
+import { Skills } from '../Skills/Skills';
+import { Education } from '../Education/Education';
+import { Links } from '../Links/Links';
 
 /*
-  To-Do
-    1. Load effect
-    2. Add links to Links
-    3. Project images
-    4. Animation-rotation through project images
-    5. Mobile compatibility for web hover function
-    6. Footer
-    7. Fix mobile view problems
-      a. Can't read intro (even worse than desktop)
-      b. Intro title not centered
-      c. Links have no outer margins
-      d. Click activates info-overly on projects, but can't remove it
+  TODO:
+  - Load effect
+  - Animation-rotation through project images
+  - Fix mobile hover for carousel buttons
+  - Education section
+  - Hover-over effect for skill icons
+  - Add effect to skip Hero upon single scroll
+  - Fix glow effect for low res screens
+  - Scroll effect
+  - Rename sections
+  - Fix problem with navbar and mockup images
+  - Find out how to combine mockup and screen images
 */
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <Hero />
       <Intro />
       <Portfolio />
+      <Skills />
+      <Education />
       <Links />
     </div>
   );
