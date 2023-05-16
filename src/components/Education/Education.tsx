040cfb767e8af32ae81351539a9c8a039f0fd1aa
ColeANiblett <coleaniblett@gmail.com>
2023-05-15 21:37:01 -0700

    add Georgia Tech attendance to education

--- src/components/Education/Education.tsx
+++ src/components/Education/Education.tsx
@@ -5,6 +5,8 @@ export const Education: React.FC = () => {
   return (
     <div className="education" id="education">
       <h6 className="education-title hidden">Education</h6>
+      <p className="education-paragtaph hidden"><em>Gerogia Tech | Atlanta, GA</em> (August 2023 - June 2025) (Expected)<br/>
+      M.S. Computer Science, GPA TBD</p>
       <p className="education-paragraph hidden"><em>De Anza College | Cupertino, CA</em> (January 2021 – March 2022)<br/>
       A.A. Systems Programming, GPA 3.87</p>
       <p className="education-paragraph hidden"><em>San Jose State University | San Jose, CA</em> (August 2012 – June 2016)<br/>
